7b2bf7db04ca365bccf1b7b5118a9ed1ee78fbca
zahidbalooshi <zahid.balooshi@gmail.com>
2025-12-21 01:09:47 +0300

    created basic routes for garages

---
+++ routes/garages.js
@@ -0,0 +1,12 @@
+const router = require("express").Router()
+const garageController = require("../controllers/garageController")
+
+
+router.get("/", garageController.getAllGarages)
+router.get("/:id", garageController.getGarageById)
+router.post("/", garageController.createGarage)
+router.put("/:id", garageController.updateGarage)
+router.delete("/:id", garageController.deleteGarage)
+
+
+module.exports = router
