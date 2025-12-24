b7c8322255f186169fd6aecef1a868a2852ab6d4
Abdulla263 <abdullafawaz8@gmail.com>
2025-12-24 22:15:11 +0300

    updated routes and models

--- models/Car.js
+++ models/Car.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose")
 
 const carSchema = new mongoose.Schema({
+  title:{type:String, required:true},
   owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User',required: true,},
   model: {type: String, required: true},
   make: {type: String, required: true},
-  year: {type: Number, required: true}
+  year: {type: Number, required: true},
+  carBrand: {type: String, required:true}
 
 })
 
